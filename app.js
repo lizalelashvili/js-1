79134ceb52656591c7bb7c978140b4fb737d9649
Liza <121138357+lizalelashvili@users.noreply.github.com>
2023-02-23 15:01:09 +0400

    finished project

---
+++ app.js
@@ -0,0 +1,29 @@
+let userName = "John";
+console.log(typeof userName);
+let age = 55;
+console.log(typeof age);
+const isCool = true;
+console.log(typeof isCool);
+const x = undefined;
+console.log(typeof x);
+const y = null;
+console.log(typeof y);
+
+const myName = "Liza";
+const myHobby = "skiing";
+const yearOfBirth = 2001;
+let currentYear = 2023;
+
+const substract = currentYear - yearOfBirth;
+console.log(substract);
+
+const sent1 =
+  "my name is " +
+  myName +
+  " I am " +
+  substract +
+  " old and my hobby is " +
+  myHobby;
+console.log(sent1);
+const sent2 = `my name is ${myName} I am ${substract} old and my hobby is ${myHobby}`;
+console.log(sent2);
